4a07227decebac39ec6bd3b4561e58811d519fc4
AlonsoLucasls <lucassilvaalonso@gmail.com>
2022-03-12 17:09:52 -0300

    Ajustes

---
+++ src/app/como-funciona/como-funciona.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'como-funciona',
+  templateUrl: './como-funciona.component.html',
+  styleUrls: ['./como-funciona.component.css'],
+})
+export class ComoFuncionaComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {
+
+  }
+}
